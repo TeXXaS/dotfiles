a48cb6e53b652139bf1adcfb179397a10a8c9797
Michal Zaborowski <michal.zaborowski@circlekeurope.com>
2018-01-29 10:04:09 +0100

    script for removing old beanstalk app versions
    Signed-off-by: Michal Zaborowski <michal.zaborowski@circlekeurope.com>

---
+++ scripts/beanstalk_purge.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import boto3
+
+eb_client = boto3.client('elasticbeanstalk')
+
+EXPECTED_COUNT = 200
+
+app_response = eb_client.describe_applications()
+for app in app_response['Applications']:
+    versions_count = len(app.get('Versions', []))
+    if versions_count < EXPECTED_COUNT:
+        print(F'{app["ApplicationName"]} skipping - found {versions_count} versions, which is less then {EXPECTED_COUNT}')
+        continue
+
+    vers_response = eb_client.describe_application_versions(ApplicationName=app['ApplicationName'])
+    versions = sorted(vers_response.get('ApplicationVersions', []), key=lambda v: v['DateCreated'])
+    print(F'{app["ApplicationName"]} removing - {versions_count} versions')
+    cnt = versions_count
+    for ver in versions:
+        print (F'removing - {VersionLabel} - {DateCreated} ')
+        response = eb_client.delete_application_version(
+            ApplicationName=ver['ApplicationName'],
+            VersionLabel=ver['VersionLabel'],
+            DeleteSourceBundle=True
+        )
+
